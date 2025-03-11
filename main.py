82a77810129f9745ce5dba8d07a41d6c2a5be7fb
PeterChrz <pc@stampedepress.org>
2025-03-10 22:01:53 -0400

    Moving Ship

--- main.py
+++ main.py
@@ -4,23 +4,33 @@
 
 import pygame
 import constants
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
     background_color = (0,0,0)
+    
 
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    while(1):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill(background_color)
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
